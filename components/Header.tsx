f008e55e5410f9938f549817fcec9ecbbc06d00d
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-08-22 18:04:16 -0300

    adding the right role to my burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import mainCss from '../styles/main.module.sass';
 const Header: NextPage = () => (
   <header id={mainCss.header}>
     <Image src="/images/logo.svg" height={100} width={100} />
-    <Image src="/images/burger.svg" height={40} width={40} />
+    <Image src="/images/burger.svg" height={40} width={40} role="menu" />
   </header>
 );
 
