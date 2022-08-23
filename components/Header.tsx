162fb1ebc8619f1bbcbe17976106d77c9c60ab8a
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-08-23 17:07:59 -0300

    styling header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,43 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { useState } from 'react';
 import useWindowsSize from '../lib/hooks/useWindowSize';
 import mainCss from '../styles/main.module.sass';
+import Menu from './header/Menu';
 
 const Header: NextPage = () => {
   const windowsSize = useWindowsSize();
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
     <header id={mainCss.header}>
       <button type="button">
-        <Image src="/images/logo.svg" height={100} width={100} />
+        <Image src="/images/logo.svg" height={100} width={100} alt="ordo realitas" />
       </button>
-      { windowsSize > 1000 && (
-        <ul>
-          <li>simulação</li>
-          <li>ocultista</li>
-          <li>combatente</li>
-          <li>especialista</li>
-        </ul>
-      ) }
-      <button type="button" role="menu">
-        <Image src="/images/burger.svg" height={40} width={40} />
+      { windowsSize > 1000 && <Menu />}
+      {!showPopup && (
+      <button
+        id="menubutton"
+        onClick={() => setShowPopup(true)}
+        type="button"
+        aria-haspopup="true"
+        aria-controls="menu"
+        aria-label="menu-button"
+      >
+        <Image
+          src="/images/burger.svg"
+          height={40}
+          width={40}
+          alt="burger menu button"
+        />
       </button>
+      )}
+      { windowsSize < 1000 && showPopup && (
+        <>
+          <Menu id="menu" role="menu" aria="menubutton" />
+          <button type="button" onClick={() => setShowPopup(false)}>X</button>
+        </>
+      )}
     </header>
   );
 };
--- cypress/component/header.cy.tsx
+++ cypress/component/header.cy.tsx
@@ -3,11 +3,27 @@
 import React from 'react';
 import Header from '../../components/Header';
 
-it('renders learn react link', async () => {
-  cy.mount(<Header />);
-  cy.viewport(360, 640);
-  cy.findByRole('banner').should('exist');
-  cy.findByRole('menu').should('exist');
-  cy.viewport(1001, 640);
-  cy.findByRole('menu').should('not exist');
+describe('Header', () => {
+  beforeEach(() => {
+    cy.viewport(360, 640);
+  });
+
+  it('changes its content in lockstep with viewport width', () => {
+    cy.mount(<Header />);
+    cy.findByRole('banner').should('exist');
+    cy.findByRole('button', {
+      name: 'menu-button',
+    }).should('exist');
+    cy.viewport(1001, 640);
+    cy.findByRole('button', {
+      name: 'menu-button',
+    }).should('not.exist');
+  });
+  it('expands the menu when on click', () => {
+    cy.mount(<Header />);
+    cy.findByRole('button', {
+      name: 'menu-button',
+    }).click();
+    cy.get('ul').contains('ocultista');
+  });
 });
