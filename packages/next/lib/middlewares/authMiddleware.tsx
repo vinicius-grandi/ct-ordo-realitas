19ece6439d0ea5bf96caa38b6500f8d2dd35bd24
vinicius-grandi <viniciusggrandi@gmail.com>
2022-11-24 19:37:34 -0300

    get translations from i18n in getServerSideProps

--- packages/next/lib/middlewares/authMiddleware.tsx
+++ packages/next/lib/middlewares/authMiddleware.tsx
@@ -2,6 +2,15 @@ import { i18n } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default async function authMiddleware(req: any) {
+  function getLocale(): string {
+    const nextSymbolKeys = Object.getOwnPropertySymbols(req);
+    const nextInitUrlSymbol = nextSymbolKeys[nextSymbolKeys.length - 1];
+    // eslint-disable-next-line no-underscore-dangle
+    const [firstPath] = req[nextInitUrlSymbol].__NEXT_INIT_URL.match(/(?<=\/)[a-z]{2}(?=\/)/) ?? [''];
+    const locale = firstPath.length > 2 || firstPath.length < 1 ? 'pt' : firstPath;
+    return locale;
+  }
+
   if (req.cookies.session) {
     return {
       redirect: {
@@ -13,7 +22,7 @@ export default async function authMiddleware(req: any) {
   if (process.env.NODE_ENV === 'development') {
     await i18n?.reloadResources();
   }
-  const props = { ...(await serverSideTranslations(req.locale ?? 'pt', ['common'])) };
+  const props = { ...(await serverSideTranslations(getLocale(), ['common'])) };
   return {
     props,
   };
